f7b768ecb0b5134d4e77ea5691d680ab542416c0
Aarohi Kondpalle <138057697+mlllemmmm@users.noreply.github.com>
2024-11-14 14:28:10 +0530

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,47 @@
+//data.js under backend
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Weeknd Tshirt',
+        category: 'Shirts',
+        image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSDKoRclZASdFXzeOV3tnJqjvgHF31ty-kn2WDG1JAHfA9iNAcze9BQ8tbeCyv2vuew0DcUxq8s8XfhLBjF695V7eLeHfrwF_8jTtXWWP4NS9aVbuVF40bqWw',
+        price: 60,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6,
+      },
+      {
+        _id: '2',
+        name: 'Oversized sweatshirt',
+        category: 'Shirts',
+        image: 'https://nobero.com/cdn/shop/files/mocha-bisque_369e451c-23a8-4ee6-8bed-a852aceeddfe.jpg?v=1702986532&width=360',
+        price: 50,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcS8jtpYnlpeVeQQZjbhveGjX9D5esJJ-4g32CywjHlSBqj3c32YtCSXxurxovqbmPNuCW_yCkQcBQhVfrFNyYAQ3_8e73PLdidyrcnDngEY',
+        price: 70,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 6,
+      }, {
+        _id: '4',
+        name: 'Track Pants',
+        category: 'Pants',
+        image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTyQjEUOMK3pXaw9kn14Q8MH0_iYSf1H2QtSi1mgX8ZxQa9pJzz1y3fDyvaKU00FyhRxR2SM_3STjlaPkMQ4ePX_KQW5cAt4cKL1pCCkgKIwGaTU2gkUFqR',
+        price: 70,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 6,
+      },
+    ]
+  }
